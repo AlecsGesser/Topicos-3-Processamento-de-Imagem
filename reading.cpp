17916f45c6ac254f1944bfea1bde0581485a003d
AlecsGesser <pasqualli.alecs@gmail.com>
2018-08-08 16:22:14 -0300

    save, read, brilho, negative working

--- reading.cpp
+++ reading.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "stdlib.h"
+#include "math.h"
  using namespace std;
 
 
@@ -14,14 +16,17 @@
  	int get(int x, int y);
  	void set(int x, int y);
  	Mat clone();
- 	~Mat();
+ 
 
  }; 
   void threshold(Mat& src, Mat& output, int value);
+  void negative(Mat& src, Mat& dst);
+  void brilho(Mat& src, Mat& dst, int value);
 
- Mat::~Mat(){
-	delete[] this->data;
-}
+
+
+
+
 
  void Mat::read(char* src){
  	FILE* fp = fopen(src, "r");
@@ -98,16 +103,13 @@
  		}
  		if(type[1] == '3'){
  			out.data = new unsigned char[out.H*out.W*3];
-			for(int i =0; i < H*W*3 ; i+=3){
+			for(int i =0; i < H*W*3 ; i++){
 				out.data[i] = this->data[i];
-				out.data[i+1] = this->data[i+1];
-				out.data[i+2] = this->data[i+2];
 			}			
  		}
  	}
 
- 	return out;
- 	free(out.data);
+ 	return out; 	
 }
 
  int Mat::get(int x, int y){
@@ -128,22 +130,29 @@
 
 int main(int argc, char** argv)
 {
- 	Mat m1, m2;
- 	//if(!argv[1]) return -1;
+ 	Mat m1,m2;
+ 	if(argc != 2){
+ 		cout<<"1 file only"<<endl;
+ 		return -1;
+ 	}
 
  	m1.read(argv[1]);
 
- 	m2 = m1.clone();
+ 	threshold(m1, m2, 50);
 
-	m2.save("vivi.pgm");
+ 	m2.save("m2");
+	
  
- 	m1.save("saida.pgm");
+ 	m1.save("saida");
+
+
 
  	return 0;
  }
 
 void threshold(Mat& src, Mat& output, int value)
 {
+	double r=200,g=200,b=100;
 	output = src.clone();
 	if(src.type[0] == 'P'){
  		if(src.type[1] == '2'){
@@ -156,14 +165,65 @@ void threshold(Mat& src, Mat& output, int value)
 			}			
  		}
  		if(src.type[1] == '3'){
-			for(int i =0; i < src.H*src.W*3 ; i+=3){
-				output.data[i] = src.data[i];
-				output.data[i+1] = src.data[i+1];
-				output.data[i+2] = src.data[i+2];
+			for(int i =0; i < src.H*src.W*3 ; i=+3){
+				double auxR, auxG, auxB;
+				auxR = (src.data[i] - r);
+				auxG = (src.data[i+1] - g);
+				auxB = (src.data[i+2] - b);
+
+				double d = pow(( pow(auxR,2) + pow(auxG,2) + pow(auxB,2)),0.5);
+
+
+				if( src.data[i] < d){
+					output.data[i] = 255;
+				}else{
+					output.data[i] = 0;
+				}
 			}			
  		}
  	}
+}
 
+void negative(Mat& src, Mat& dst){
+	dst = src.clone();
+	if(src.type[0] == 'P'){
+ 		if(src.type[1] == '2'){
+			for(int i =0; i < src.H*src.W ; i++){						
+					dst.data[i] =  255 - src.data[i];
+			}			
+ 		}
+ 		if(src.type[1] == '3'){
+			for(int i =0; i < src.H*src.W*3 ; i+=3){
+				dst.data[i] = 255 - src.data[i];
+				dst.data[i+1] = 255 - src.data[i+1];
+				dst.data[i+2] = 255 - src.data[i+2];
+			}			
+ 		}
+ 	}
+}
 
-
+void brilho(Mat& src, Mat& dst, int value){
+	dst = src.clone();
+	if(src.type[0] == 'P'){
+ 		if(src.type[1] == '2'){
+			for(int i =0; i < src.H*src.W ; i++){
+				double aux = src.data[i] + value;
+				if(aux >= 255){
+					dst.data[i] = 255;	
+				}else{
+					dst.data[i] =  src.data[i] + value;
+				}
+			}			
+ 		}
+ 		if(src.type[1] == '3'){
+			for(int i =0; i < src.H*src.W*3 ; i++){
+				double aux = src.data[i] + value;
+				if(aux >= 255){
+					dst.data[i] = 255;	
+				}else{
+					dst.data[i] =  src.data[i] + value;
+				}
+			}			
+ 		}
+ 	}
 }
